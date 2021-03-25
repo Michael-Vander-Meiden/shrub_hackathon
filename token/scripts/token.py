87c22af0b6807650fdee2dfde1f17f5d8894057d
Michael Vander Meiden <michael@newusers-MacBook-Pro.local>
2021-03-24 21:28:13 -0400

    first commit

---
+++ token/scripts/token.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from brownie import Token, accounts
+
+
+def main():
+    return Token.deploy("Token A", "TOKA", 18, 1e21, {'from': accounts[0]})
+    return Token.deploy("Token B", "TOKB", 18, 1e21, {'from': accounts[0]})
