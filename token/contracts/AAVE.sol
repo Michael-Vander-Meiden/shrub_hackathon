88dc802c24b7b4355ffd6f5f0e0709e17ceb63fa
Daniel Bolis <daniel@Daniels-MBP-2.home>
2021-04-03 16:55:14 -0400

    one more change to last commit

--- token/contracts/AAVE.sol
+++ token/contracts/AAVE.sol
@@ -26,9 +26,9 @@ function deposit() public payable {
 
     }
 
-function depositToken(uint256 _amount) public payable {
+function depositDAI(uint256 _amount) public payable {
     LendingPool lendingpool = LendingPool(provider.getLendingPool());
-    address Token = address(0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD); //Needs to be dai address
+    address Token = address(0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD); //DAI Kovan address 
     lendingpool.deposit(Token,_amount,address(this),0);
 }
 
